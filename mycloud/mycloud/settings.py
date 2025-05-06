07ba74c22565fe541f77ba4bfbd01b75fafd0080
PrekinII <prekinivan@gmail.com>
2025-05-06 22:52:39 +0500

    remove port 3000

--- mycloud/mycloud/settings.py
+++ mycloud/mycloud/settings.py
@@ -102,22 +102,22 @@ STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
 
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    # "http://localhost:3000",
+    # "http://127.0.0.1:3000",
     "http://95.163.223.166",
-    "http://95.163.223.166:3000",  # Добавляем порт явно
+    # "http://95.163.223.166:3000",  # Добавляем порт явно
     "https://95.163.223.166",      # Для HTTPS, если будет
-    "https://95.163.223.166:3000"
+    # "https://95.163.223.166:3000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    # "http://localhost:3000",
+    # "http://127.0.0.1:3000",
     "http://95.163.223.166",
-    "http://95.163.223.166:3000",
+    # "http://95.163.223.166:3000",
     "https://95.163.223.166",
-    "https://95.163.223.166:3000"
+    # "https://95.163.223.166:3000"
 ]
 
 # Session settings
